9c64bfc9b60011550e41c5af5c26870a3e861b63
Guillermo Crescencio <guillermo.crescencio@laureate.net>
2018-01-04 14:52:56 -0600

    Fixing import error.

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,7 +1,7 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/common/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { ForInstitutionsComponent } from './for-institutions/for-institutions.component';
 
