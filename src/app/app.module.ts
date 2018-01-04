f34b20566dd556d2463df080db0c3396af0a83cb
Cristian Turcios <cturcioscolindres38@yahoo.com>
2018-01-04 14:47:35 -0600

    scaffolding help and support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,25 @@ import { RoutesContainer, RoutesProvider } from './app.router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { SupportComponent } from './components/support/support.component';
+import { SupportIntroComponent } from './components/support/support-intro/support-intro.component';
+import { SupportDescriptionComponent } from './components/support/support-description/support-description.component';
+import { SuccessTipsComponent } from './components/support/success-tips/success-tips.component';
+import { SupportUniversitiesComponent } from './components/support/support-universities/support-universities.component';
+import { SupportClaudioDirective } from './components/support/support-claudio.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    SupportComponent,
+    SupportIntroComponent,
+    SupportDescriptionComponent,
+    SuccessTipsComponent,
+    SupportUniversitiesComponent,
+    SupportClaudioDirective
   ],
   imports: [
     BrowserModule,
--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -3,9 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { SupportComponent } from './components/support/support.component';
+
 
 const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'support', component: SupportComponent},    
     {path: '**', component: HomeComponent}
 ];
 
