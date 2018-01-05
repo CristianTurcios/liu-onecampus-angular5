156fbbe5bcee671007ee03a6487d1f6a5671f14e
Handerson Contreras <hcontreras@hnsc.net>
2018-01-04 21:56:21 -0600

    refactoring code to group component in a module

---
+++ src/app/marketing/intro/intro.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { WagtailProvider } from '../../providers/cms.providers';
+
+@Component({
+    selector: 'app-home-intro',
+    templateUrl: './intro.component.html',
+    providers: [WagtailProvider]
+})
+export class HomeIntroComponent implements OnInit {
+    public introPage: any = '';
+
+    constructor (
+        private _pagesProvider: WagtailProvider
+    ) {}
+
+    ngOnInit() {
+        this._pagesProvider.getPage('home-intro', 'home.Video').subscribe(result => {
+            if (result.items.length === 1) {
+                this.introPage = result.items[0];
+            }
+        }, error => {
+            const errorMessage = <any>error;
+            console.log(errorMessage);
+        });
+    }
+
+}
